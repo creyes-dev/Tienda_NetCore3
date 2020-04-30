2d8f1c1773e96a7fdc2f699bae48a4986e807f7e
Cristian <creyes.dev@gmail.com>
2020-04-30 18:06:19 -0300

    Mover el contenido de Data
    Mover el contenido de Data a la capa de AccesoDatos

--- Tienda_NetCore3/Tienda_NetCore3.AccesoDatos/Data/ApplicationDbContext.cs
+++ Tienda_NetCore3/Tienda_NetCore3.AccesoDatos/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Tienda_NetCore3.Data
+namespace Tienda_NetCore3.AccesoDatos.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
