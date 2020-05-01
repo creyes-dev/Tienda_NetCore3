51ec539daa24b840ea9061ac381907f378b2de66
Cristian <creyes.dev@gmail.com>
2020-05-01 00:28:22 -0300

    Crear tabla Categoria a partir de la clase Categoria

---
+++ Tienda_NetCore3/comandos.sh
@@ -0,0 +1,9 @@
+PM> add-migration AddCategoriaToDatabase
+Build started...
+Build succeeded.
+To undo this action, use Remove-Migration.
+
+PM> update-database
+Build started...
+Build succeeded.
+Done.
