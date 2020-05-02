8ebd0469614446bd638ad64d87ea5a4583684a6e
Cristian <creyes.dev@gmail.com>
2020-05-02 00:52:51 -0300

    Insertar y modificar categorias
    dataTables no está mostrando los datos de las categorías registradas

--- Tienda_NetCore3/Tienda_NetCore3/Areas/Admin/Controllers/CategoriaController.cs
+++ Tienda_NetCore3/Tienda_NetCore3/Areas/Admin/Controllers/CategoriaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Tienda_NetCore3.AccesoDatos.Data.Repository;
+using Tienda_NetCore3.Models;
 
 namespace Tienda_NetCore3.Areas.Admin.Controllers
 {
@@ -21,5 +22,66 @@ namespace Tienda_NetCore3.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult Upsert(int? id)
+        {
+            Categoria categoria = new Categoria();
+            if(id == null)
+            {
+                return View(categoria);
+            }
+            categoria = _unitOfWork.categoria.Get(id.GetValueOrDefault());
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Categoria categoria)
+        {
+            if(ModelState.IsValid)
+            {
+                if(categoria.Id == 0)
+                {
+                    _unitOfWork.categoria.Add(categoria);
+                }
+                else
+                {
+                    _unitOfWork.categoria.Update(categoria);
+                }
+                _unitOfWork.AplicarCambios();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult ObtenerTodas()
+        {
+            return Json(new { data = _unitOfWork.categoria.ObtenerListadoCategorias() });
+        }
+
+        [HttpDelete]
+        public IActionResult Eliminar(int id)
+        {
+            var categoria = _unitOfWork.categoria.Get(id);
+            if(categoria == null)
+            {
+                return Json(new { success = false, message = "Error al eliminar la categoría" });
+            }
+
+            _unitOfWork.categoria.Remove(categoria);
+            _unitOfWork.AplicarCambios();
+
+            return Json(new { success = true, message = "La categoría seleccionada ha sido eliminada" });
+        }
+
+        #endregion
+
     }
 }
---
+++ Tienda_NetCore3/Tienda_NetCore3/wwwroot/js/Categoria.js
@@ -0,0 +1,39 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Categoria/ObtenerTodas",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "Nombre", "width": "50%" },
+            { "data": "Orden", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/Admin/Categoria/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-edit'></i> Editar
+                                </a>
+                                &nbsp;
+                                <a onclick=Delete("/Admin/Categoria/Eliminar/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                                    <i class='far fa-trash-alt'></i> Eliminar
+                                </a>
+                            </div>
+                            `;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "Sin registros."
+        },
+        "width": "100%"
+    });
+}
